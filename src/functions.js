aeadf2c5e05f0d8b26b18cefe6ce8031e9bc4b31
sadmin.storozhenko@just-ai.com <sadmin.storozhenko@just-ai.com>
2022-12-21 13:26:30 +0000

    Edited files:     /src/functions.js

--- src/functions.js
+++ src/functions.js
@@ -28,69 +28,69 @@ function getHypothesesForButtons(nBest) {
 
 } 
 
-// function getCurrentWeather (lat, lon) {
+function getCurrentWeather (lat, lon) {
     
-//     var apiKey = $jsapi.context().injector.weatherApiKey; 
+    var apiKey = $jsapi.context().injector.weatherApiKey; 
     
-//     var response = $http.get("http://api.openweathermap.org/data/2.5/weather?APPID=${APPID}&units=${units}&lang=${lang}&lat=${lat}&lon=${lon}"), { 
-
-//             timeout: 10000, 
-//             query:{ 
-//                 APPID: apiKey, 
-//                 units: "metric", 
-//                 lang: "ru", 
-//                 lat: lat, 
-//                 lon: lon
-//             } 
-        
-//     }); 
-//     if (!response.isOk || !response.data) { 
-//         return false; 
-//      } 
-//     var weather = {}; 
-//     weather.temp = response.data.main.temp; 
-//     weather.description = response.data.weather[0].description; 
-//     return weather; 
-    
-// } 
-
-
-function getCurrentWeather(lat, lon) { 
-
-var apiKey = $jsapi.context().injector.weatherApiKey; 
-
-var response = $http.get("http://api.openweathermap.org/data/2.5/weather?APPID=${APPID}&units=${units}&lang=${lang}&lat=${lat}&lon=${lon}", { 
+    var response = $http.get("http://api.openweathermap.org/data/2.5/weather?APPID=${APPID}&units=${units}&lang=${lang}&lat=${lat}&lon=${lon}", { 
 
             timeout: 10000, 
-
             query:{ 
-
                 APPID: apiKey, 
-
                 units: "metric", 
-
                 lang: "ru", 
-
                 lat: lat, 
-
-                lon: lon 
-
+                lon: lon
             } 
-
-        }); 
-
-if (!response.isOk || !response.data) { 
-
-return false; 
-
+        
+    }); 
+    if (!response.isOk || !response.data) { 
+        return false; 
+     } 
+    var weather = {}; 
+    weather.temp = response.data.main.temp; 
+    weather.description = response.data.weather[0].description; 
+    return weather; 
+    
 } 
 
-var weather = {}; 
 
-weather.temp = response.data.main.temp; 
+// function getCurrentWeather(lat, lon) { 
 
-weather.description = response.data.weather[0].description; 
+// var apiKey = $jsapi.context().injector.weatherApiKey; 
 
-return weather; 
+// var response = $http.get("http://api.openweathermap.org/data/2.5/weather?APPID=${APPID}&units=${units}&lang=${lang}&lat=${lat}&lon=${lon}", { 
 
-} 
+//             timeout: 10000, 
+
+//             query:{ 
+
+//                 APPID: apiKey, 
+
+//                 units: "metric", 
+
+//                 lang: "ru", 
+
+//                 lat: lat, 
+
+//                 lon: lon 
+
+//             } 
+
+//         }); 
+
+// if (!response.isOk || !response.data) { 
+
+// return false; 
+
+// } 
+
+// var weather = {}; 
+
+// weather.temp = response.data.main.temp; 
+
+// weather.description = response.data.weather[0].description; 
+
+// return weather; 
+
+// } 
